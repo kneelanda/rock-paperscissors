f2c5b9695e18dc95296fb255234c283324b8ada1
kneelanda <ashlin.jb.kneeland@gmail.com>
2022-07-12 16:53:52 -0700

    added user choice

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
 print("Rock Paper Scissors Game")
+
+#User inputs
+
+user_choice = input("Please make a choice('rock','paper','scissors'): ")
+print("You chose: " + user_choice)
+
+#Validate user inputs 
+
+
+#Computer choice
+
+
+#Determine the winner
+
+
+
+#Display results 
+
+#
+# 
