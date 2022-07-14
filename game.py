b25dd9709f83d685004010fd17808ef2a4cd2497
kneelanda <ashlin.jb.kneeland@gmail.com>
2022-07-13 18:00:13 -0700

    Updated user inputs

--- game.py
+++ game.py
@@ -5,12 +5,14 @@ import random
 #User inputs
 
 user_choice = input("Please make a choice('rock','paper','scissors'): ")
+user_choice=user_choice.lower()
 print("You chose: " + user_choice)
 
 computer_choice = random.choice(['rock' , 'paper' , 'scissors'])
 print("The computer chose: " + computer_choice)
 
 #Validate user inputs 
+
 
 
 #Computer choice
